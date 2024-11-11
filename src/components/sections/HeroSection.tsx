2fa49f11a3b12a8a623f7b62255cb1c7717cae19
Amadeus J <dawoonjung1026@gmail.com>
2024-11-11 17:48:29 +0900

    타이틀 애니메이션 추가

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -12,20 +12,52 @@ export default function HeroSection() {
     console.log('HeroSection mounted');
   }, []);
 
+  const bounceUp = {
+    hover: {
+      y: -10,
+      transition: {
+        type: 'spring',
+        stiffness: 1000, // 탄성
+        damping: 15, // 감쇠
+        mass: 0.5, // 질량
+      },
+    },
+  };
+
   return (
     <section className={`section ${styles.heroSection}`}>
       <AeroCard className={`${styles.heroCard}`}>
         <div className={styles.heroLeft}>
           <div className={styles.heroTitle}>
+            {/* 메인 타이틀 (직업) */}
             <motion.h1
               className={styles.heroMainTitle}
             >
-              Frontend Developer
+              {'Frontend Developer'.split('').map((char, index) => (
+                <motion.span
+                  key={index}
+                  variants={bounceUp}
+                  whileHover="hover"
+                  style={{ display: 'inline-block', marginRight: char === ' ' ? '8px' : '0' }}
+                >
+                  {char}
+                </motion.span>
+              ))}
             </motion.h1>
+            {/* 서브 타이틀 (이름) */}
             <motion.h1
               className={styles.heroSubtitle}
             >
-              JUNG DAWOON
+              {'Jung Dawoon'.split('').map((char, index) => (
+                <motion.span
+                  key={index}
+                  variants={bounceUp}
+                  whileHover="hover"
+                  style={{ display: 'inline-block', marginRight: char === ' ' ? '8px' : '0' }}
+                >
+                  {char}
+                </motion.span>
+              ))}
             </motion.h1>
           </div>
           <div className={styles.heroContent}>
