bd64e9e0c2920ef613c6ed596320fc10bde4dc44
Amadeus J <dawoonjung1026@gmail.com>
2024-11-18 16:32:46 +0900

    media scss

---
+++ src/components/header/CategoryHeader.tsx
@@ -0,0 +1,9 @@
+// 카테고리 헤더
+
+import React from 'react';
+
+import styles from './CategoryHeader.module.scss';
+
+export default function CategoryHeader() {
+  return <div className={styles.categoryHeader}>Category Header</div>;
+}
--- src/components/sections/CareerSection.tsx
+++ src/components/sections/CareerSection.tsx
@@ -2,18 +2,20 @@
 
 import React from 'react';
 import AeroCard from '../Aero/AeroCard';
+import { motion } from 'framer-motion';
 import styles from './CareerSection.module.scss';
 
 export default function CareerSection() {
   return (
     <section className={`section ${styles.careerSection}`}>
       <AeroCard className={styles.careerCard}>
-        <h1>CareerSection</h1>
-        <h1>CareerSection</h1>
-        <h1>CareerSection</h1>
-        <h1>CareerSection</h1>
-        <h1>CareerSection</h1>
-        <h1>CareerSection</h1>
+        <div className={styles.careerHeader}>
+        </div>
+        <div className={styles.careerBody}>
+          <motion.div>
+            <motion.img src="/images/career-map.png" alt="career-main" />
+          </motion.div>
+        </div>
       </AeroCard>
     </section>
   );
