18cba48c66fdb3f603889070f018c1742a218ae8
Amadeus J <dawoonjung1026@gmail.com>
2024-12-04 14:33:30 +0900

    projects nav head design

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,16 +4,67 @@ import React from 'react';
 import { useRouter } from 'next/navigation';
 import AeroCard from '../Aero/AeroCard';
 import styles from './Header.module.scss';
+import { motion } from 'framer-motion';
 
 export default function Header() {
   const router = useRouter();
+  const expandEffect = {
+    hover: {
+      scale: 1.3,
+      transition: {
+        type: 'spring',
+        stiffness: 300,
+        damping: 15,
+      },
+    },
+  };
 
   return (
     <header className={styles.categoryHeader}>
       <AeroCard className={styles.categoryHeaderCard}>
-        <button onClick={() => router.push('/')}>Main</button>
-        <button onClick={() => router.push('/projects')}>Projects</button>
-        <button onClick={() => router.push('/about')}>About Me</button>
+        <button onClick={() => router.push('/')}>
+          <motion.img
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            width={60}
+            height={60}
+            variants={expandEffect}
+            whileHover="hover"
+            src="/icons/home-button2.svg"
+            alt="Home"
+          />
+        </button>
+        <button onClick={() => router.push('/projects')}>
+          <motion.img
+            variants={expandEffect}
+            whileHover="hover"
+            width={60}
+            height={60}
+            src="/icons/keynote.svg"
+            alt="Projects"
+          />
+        </button>
+        <button>
+          <motion.img
+            variants={expandEffect}
+            whileHover="hover"
+            width={65}
+            height={65}
+            src="/icons/joystick2.svg"
+            alt="Contact"
+          />
+        </button>
+        <button onClick={() => router.push('/about')}>
+          <motion.img
+            variants={expandEffect}
+            whileHover="hover"
+            width={60}
+            height={60}
+            src="/icons/id-card.svg"
+            alt="About"
+          />
+        </button>
       </AeroCard>
     </header>
   );
