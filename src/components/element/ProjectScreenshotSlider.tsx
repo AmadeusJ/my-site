d56d9d2cf9dc3ae7e65cbb40497181e7c78c2ee3
downeyJ <dawoonjung1026@gmail.com>
2024-12-12 22:49:45 +0900

    slider modal add

--- src/components/element/ProjectScreenshotSlider.tsx
+++ src/components/element/ProjectScreenshotSlider.tsx
@@ -1,9 +1,10 @@
 // 프로젝트 상세 페이지 - 프로젝트 스크린샷 슬라이더
 // src/components/element/ProjectScreenshotSlider.tsx
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import Modal from 'react-modal';
 
 import styles from './ProjectScreenshotSlider.module.scss';
 
@@ -16,27 +17,77 @@ type ImageSliderProps = {
   images: string[]; // Array of image paths
 };
 
+// Modal styles
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    padding: 0,
+    border: 'none',
+    background: 'transparent',
+  },
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+  },
+};
+
 export default function ProjectScreenshotSlider({ images }: ImageSliderProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const openModal = (image: string) => {
+    setSelectedImage(image);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedImage(null);
+    setIsModalOpen(false);
+  };
+
   return (
-    <Swiper
-      className={styles.swiper}
-      modules={[Autoplay, Navigation, Pagination]}
-      spaceBetween={10}
-      slidesPerView={1}
-      autoplay={{ delay: 3800, disableOnInteraction: false, }}
-      loop={true}
-      pagination={{ clickable: true }}
-      speed={1000}
-    >
-      {images.map((image, index) => (
-        <SwiperSlide key={index} className={styles.swiperSlide}>
+    <>
+      <Swiper
+        className={styles.swiper}
+        modules={[Autoplay, Navigation, Pagination]}
+        spaceBetween={10}
+        slidesPerView={1}
+        autoplay={{ delay: 3800, disableOnInteraction: false, }}
+        loop={true}
+        pagination={{ clickable: true }}
+        speed={1000}
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={index} className={styles.swiperSlide}>
+            <img
+              src={image.path}
+              alt={`Slide ${index + 1}`}
+              style={{ width: image.width, height: image.height, borderRadius: '8px' }}
+              onClick={() => openModal(image.path)} // Open modal on image click
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      {/* Modal for zoomed-in image */}
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Zoomed Image"
+        ariaHideApp={false}
+      >
+        {selectedImage && (
           <img
-            src={image.path}
-            alt={`Slide ${index + 1}`}
-            style={{ width: image.width, height: image.height, borderRadius: '8px' }}
+            src={selectedImage}
+            alt="Zoomed"
+            style={{ width: '100%', height: 'auto', borderRadius: '8px' }}
           />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+        )}
+      </Modal>
+    </>
   );
 };
