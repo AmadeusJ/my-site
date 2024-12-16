8ef0b3faa1040d99ea55388c208cb155cfbf28d8
Amadeus J <dawoonjung1026@gmail.com>
2024-12-16 17:24:06 +0900

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
-import { nextui } from "@nextui-org/react";
+// tailwind.config.js
+
+const { nextui } = require("@nextui-org/react");
 
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
-    // 추가로 Tailwind가 스캔해야 할 경로
+    './app/**/*.{js,ts,jsx,tsx,mdx}', // Next.js App Router 경로
+    './pages/**/*.{js,ts,jsx,tsx,mdx}', // Next.js Pages Router 경로
+    './components/**/*.{js,ts,jsx,tsx,mdx}', // Components 경로
+    './src/**/*.{js,ts,jsx,tsx,mdx}', // src 디렉토리 내의 모든 파일
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}', // NextUI 테마 관련 파일
   ],
   theme: {
-    extend: {},
+    extend: {}, // 커스텀 확장은 필요 시 추가
   },
-  plugins: [nextui()], // NextUI 플러그인 추가
+  plugins: [
+    nextui(), // NextUI 플러그인 등록
+  ],
 };
